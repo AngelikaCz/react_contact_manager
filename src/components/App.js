f16c47dbce1c56505ae78277d957216e0d9f76ea
AngelikaCz <angelikaczerwinska9@gmail.com>
2022-11-24 19:25:36 +0100

    Added Header html

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import "./App.css";
-import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
+import Header from "./Header";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
-      <AddContact />
-      <ContactList />
     </div>
   );
 }
