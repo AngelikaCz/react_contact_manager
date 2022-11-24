7851dd29632cb4358a80b12f466639136c5c48af
AngelikaCz <angelikaczerwinska9@gmail.com>
2022-11-24 19:03:40 +0100

    Imported components

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,15 @@
+import React from "react";
 import "./App.css";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
 
 export default function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Header />
+      <AddContact />
+      <ContactList />
+    </div>
+  );
 }
